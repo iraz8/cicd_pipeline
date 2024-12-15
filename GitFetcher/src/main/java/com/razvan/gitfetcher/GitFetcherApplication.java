8d737011c67c9bd4cbab21cdba3f5d1a81d569bc
Razvan <irazvan1995@gmail.com>
2024-12-15 21:02:03 +0200

    Initial commit

---
+++ GitFetcher/src/main/java/com/razvan/gitfetcher/GitFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.razvan.gitfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFetcherApplication.class, args);
+	}
+
+}
